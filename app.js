f9b8c3c367f6d67327cfaf9f83ca265867616c5f
sadh123 <45258058+sadh123@users.noreply.github.com>
2020-10-20 18:38:19 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express =require("express");
+let bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000
+
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
+//-----/API/first  
+app.get("/api/first", (req, res) => {
+ console.log(req.query.query)
+  res.send('First Successfull');
+});
+
+//-----/API/second  
+app.get("/api/second", (req, res) => {
+  console.log(req.query.query)
+   res.send('Second Successfull');
+ });
+ //-----/API/third  
+app.get("/api/third", (req, res) => {
+  console.log(req.query.query)
+   res.send('Third Successfull');
+ });
+ //-----/API/fourth  
+app.get("/api/fourth", (req, res) => {
+  console.log(req.query.query)
+  
+   res.status(200).send('Fourth Successfull');
+ });
+
+
+app.listen(PORT, () => { console.log('server started at http://localhost:'+PORT+')' })
+
